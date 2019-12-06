320edfe5db8eab5af0554cf0726cd9be3c48bddc
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2019-12-06 13:52:47 +0800

    视图生成提交

--- __openerp__.py
+++ __openerp__.py
@@ -1,15 +1,20 @@
 {
     'name': 'Tase Queue',
     'depends': [
-        'base'
+        'base', 'base_setup'
     ],
     'author': '',
     'data': [
-        'views.xml'
+        #'data/ir_cron_datas.xml',
+        'security/ir.model.access.csv',
+
+        'views/oe_task_views.xml',
+        'views/oe_task_result_views.xml',
+        #'views.xml'
     ],
     'installable': True,
     'application': False,
     'external_dependencies': {
-        'python': ['celery'],
+        'python': [],
     }
 }
