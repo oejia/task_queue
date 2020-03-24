a59093b02f8669600d7fd59e5427a29676b8c977
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2020-03-24 12:18:57 +0800

    增加失败任务的重新执行

--- models/task_result.py
+++ models/task_result.py
@@ -14,3 +14,15 @@ class TaskResult(models.Model):
     result = fields.Html(_('result'), default=None)
     date_done = fields.Datetime('done at', default=fields.Datetime.now)
     traceback = fields.Text(_('traceback'))
+
+    @api.multi
+    def re_execute(self):
+        for obj in self:
+            task = self.env['oe.task'].sudo().create({
+                'task_id': '',
+                'task_name': obj.task_name,
+                'task_args': obj.task_args,
+                'task_kwargs': obj.task_kwargs,
+                'countdown': 0,
+            })
+            obj.write({'status': 'RETRY'})
